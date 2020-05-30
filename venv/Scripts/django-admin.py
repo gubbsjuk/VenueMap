f4c8648017e465ed7e4633e47262deefaab3c8e8
Gubbsjuk <joachimleine@gmail.com>
2020-05-30 22:17:47 +0200

    Opprydning

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\joach\onedrive\dokumenter\github repo\imagemap\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
