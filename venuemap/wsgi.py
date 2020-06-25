73b32d354d2e12ce584dab618d14c51efa1a0df0
Gubbsjuk <joachimleine@gmail.com>
2020-06-25 14:33:37 +0200

    remove production specific code.

--- venuemap/wsgi.py
+++ venuemap/wsgi.py
@@ -9,11 +9,6 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from project_setup import main as project_setup_main
-
-if __name__ == '__main__':
-    project_setup_main()
-    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yoursettings')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'venuemap.settings')
 
